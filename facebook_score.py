8f404b94f801edfc65c57c110a96133db0eda4aa
StephAtayen <sc@atayen.us>
2021-05-11 11:48:48 +0200

    import global config

--- facebook_score.py
+++ facebook_score.py
@@ -1,160 +1,186 @@
 from pymongo import MongoClient
 from scipy.spatial import distance
+import config
 
-
-##### Script de calcul du score sur la plateforme Facebook
-#### Connection au cluster :
-client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+# Script de calcul du score sur la plateforme Facebook
+# Connection au cluster :
+client = MongoClient(config.MONGO_PASSPHRASE)
 
 db = client["database"]
 collection = db["page_fb_stats"]
 page_fb_collection = db["page_fb"]
 influence_account = db["influence_account"]
 
-#### Affectation A une division en fonction du nombre de followers :
+# Affectation A une division en fonction du nombre de followers :
 
 
 division = [0, 1000, 10000, 50000, 500000, 1000000]
 
 for result in collection.find():
 
-        for i in range(len(division)-1):
+    for i in range(len(division)-1):
 
-            if result["page_fans"] >= division[i] and result["page_fans"] < division[i+1] :
+        if result["page_fans"] >= division[i] and result["page_fans"] < division[i+1]:
+
+            collection.update({"_id": result['_id']}, {
+                              "$set": {"division": i+1}})
+            influence_account.update_one({"page_fb": result["_id"]}, {
+                                         "$set": {"facebook_division": i+1}})
+
+    if result["page_fans"] > division[len(division) - 1]:
+        collection.update({"_id": result['_id']}, {
+                          "$set": {"division": len(division)}})
+        influence_account.update_one({"page_fb": result["_id"]}, {
+                                     "$set": {"facebook_division": len(division)}})
 
 
-                collection.update({"_id": result['_id']}, {"$set": {"division": i+1}})
-                influence_account.update_one({"page_fb": result["_id"]}, {"$set": {"facebook_division": i+1}})
-
-        if result["page_fans"] > division[len(division) - 1]:
-            collection.update({"_id": result['_id']}, {"$set": {"division": len(division)}})
-            influence_account.update_one({"page_fb": result["_id"]}, {"$set": {"facebook_division": len(division)}})
-
-
-    #### ajout de la metrique page_engagement_rate
+# ajout de la metrique page_engagement_rate
 
 for result in collection.find():
     try:
         engaged_users = result["page_engaged_users"]
         nb_fans = result["page_fans"]
         tx = engaged_users / nb_fans
-        collection.update_one({"_id": result['_id']}, {"$set": {"page_engagement_rate": tx}})
-        influence_account.update_one({"": result["_id"]}, {"$set": {"facebook_engagement_rate": round(tx, 2)}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"page_engagement_rate": tx}})
+        influence_account.update_one(
+            {"": result["_id"]}, {"$set": {"facebook_engagement_rate": round(tx, 2)}})
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"page_engagement_rate": 0}})
-        influence_account.update_one({"": result["_id"]}, {"$set": {"facebook_engagement_rate": 0}})
-    #### ajout de la mÃƒÂ©trique page_posts_engagement_rate
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"page_engagement_rate": 0}})
+        influence_account.update_one(
+            {"": result["_id"]}, {"$set": {"facebook_engagement_rate": 0}})
+    # ajout de la metrique page_posts_engagement_rate
 
     try:
         page_engagements = result["page_post_engagements"]
         page_impressions = result["page_posts_impressions"]
         tx = page_engagements / page_impressions
-        collection.update_one({"_id": result['_id']}, {"$set": {"page_posts_engagement_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"page_posts_engagement_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"page_posts_engagement_rate": 0}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"page_posts_engagement_rate": 0}})
 
-    #### ajout de la metrique reach_rate
+    # ajout de la metrique reach_rate
 
     try:
         page_impressions_unique = result["page_impressions_unique"]
         nb_fans = result["page_fans"]
-        influence_account.update_one({"page_fb": result["_id"]}, {"$set": {"facebook_reach": page_impressions_unique}})
-        influence_account.update_one({"page_fb": result["_id"]}, {"$set": {"facebook_fans": nb_fans}})
+        influence_account.update_one({"page_fb": result["_id"]}, {
+                                     "$set": {"facebook_reach": page_impressions_unique}})
+        influence_account.update_one({"page_fb": result["_id"]}, {
+                                     "$set": {"facebook_fans": nb_fans}})
         tx = page_impressions_unique / nb_fans
-        collection.update_one({"_id": result['_id']}, {"$set": {"reach_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"reach_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"reach_rate": 0}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"reach_rate": 0}})
 
-    #### ajout de la metrique fan_adds_rate
+    # ajout de la metrique fan_adds_rate
 
     try:
         nb_fans_add = result["page_fan_adds_unique"]
         nb_fans = result["page_fans"]
         tx = nb_fans_add / nb_fans
-        collection.update_one({"_id": result['_id']}, {"$set": {"fan_adds_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"fan_adds_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"fan_adds_rate": 0}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"fan_adds_rate": 0}})
 
-    #### ajout de la mÃƒÂ©trique fan_removes_rate
+    # ajout de la metrique fan_removes_rate
 
     try:
         nb_fans_remove = result["page_fan_removes_unique"]
         nb_fans = result["page_fans"]
         tx = nb_fans_remove / nb_fans
-        collection.update_one({"_id": result['_id']}, {"$set": {"fan_removes_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"fan_removes_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"fan_removes_rate": 0}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"fan_removes_rate": 0}})
 
-    #### ajout de la mÃƒÂ©trique fan_adds_removes_rate
+    # ajout de la metrique fan_adds_removes_rate
 
     try:
         nb_fans_remove = result["page_fan_removes_unique"]
         nb_fans_add = result["page_fan_adds_unique"]
         tx = nb_fans_remove / nb_fans_add
-        collection.update_one({"_id": result['_id']}, {"$set": {"fan_adds_removes_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"fan_adds_removes_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"fan_adds_removes_rate": 0}})
-### ajout des mÃ©triques sur vidÃ©o:
-    ## ajout du taux : vus_unique/vus
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"fan_adds_removes_rate": 0}})
+# ajout des metrique sur video:
+    # ajout du taux : vus_unique/vus
     try:
         vus = result["page_video_views"]
         vus_unique = result["page_video_views_unique"]
         tx = vus_unique / vus
-        collection.update_one({"_id": result['_id']}, {"$set": {"vu_unique_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"vu_unique_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"vu_unique_rate": 0}})
-    ## ajout du taux : vus_click_to_play/vus
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"vu_unique_rate": 0}})
+    # ajout du taux : vus_click_to_play/vus
     try:
         vus = result["page_video_views"]
         vus_click_to_play = result["page_video_views_click_to_play"]
         tx = vus_click_to_play / vus
-        collection.update_one({"_id": result['_id']}, {"$set": {"vu_click_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"vu_click_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"vu_click_rate": 0}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"vu_click_rate": 0}})
 
-    ## ajout du taux : complete_views_30s/views
+    # ajout du taux : complete_views_30s/views
 
     try:
         vus = result["page_video_views"]
         vus_complete_30 = result["page_video_complete_views_30s"]
         tx = vus_complete_30 / vus
-        collection.update_one({"_id": result['_id']}, {"$set": {"vu_complete_30s_rate": tx}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"vu_complete_30s_rate": tx}})
 
     except:
 
-        collection.update_one({"_id": result['_id']}, {"$set": {"vu_complete_30s_rate": 0}})
+        collection.update_one({"_id": result['_id']}, {
+                              "$set": {"vu_complete_30s_rate": 0}})
 
-    ### ajout de verification_status
+    # ajout de verification_status
 
     for doc in page_fb_collection.find():
-        ### codage de verification_status : 1 indique verified et 0 indque non verified
+        # codage de verification_status : 1 indique verified et 0 indque non verified
         if doc["id"] == result["id"]:
             verification_status = 0
             if doc["verification_status"] != "not_verified":
                 verification_status = 1
 
-            collection.update_one({"_id": result['_id']}, {"$set": {"verification_status": verification_status}})
+            collection.update_one({"_id": result['_id']}, {
+                                  "$set": {"verification_status": verification_status}})
 
-#### Normalisation des variables utilisÃƒÂ©es pour le calcul du score
+# Normalisation des variables utilisees pour le calcul du score
 #### Normalisation : (x - min) / (max - min)
 kpis = ["page_fans", "page_negative_feedback_unique", "page_engagement_rate",
         "page_posts_engagement_rate", "reach_rate", "fan_adds_rate",
-        "fan_adds_removes_rate", "verification_status","page_video_views","vu_unique_rate","vu_click_rate","vu_complete_30s_rate"]
+        "fan_adds_removes_rate", "verification_status", "page_video_views", "vu_unique_rate", "vu_click_rate", "vu_complete_30s_rate"]
 #division = [1, 2, 3, 4, 5, 6, 7, 8]
 collection = db["page_fb_stats"]
 for kpi in kpis:
@@ -170,7 +196,7 @@ for kpi in kpis:
             }
             }
 
-        ];
+        ]
 
         if len(list(collection.aggregate(pipeline))) != 0:
 
@@ -184,16 +210,17 @@ for kpi in kpis:
                                           {"$set": {kpi + "_normal": (user[kpi] - min) / (max - min)}})
 
                 else:
-                    collection.update_one({"_id": user['_id']}, {"$set": {kpi + "_normal": 0}})
+                    collection.update_one({"_id": user['_id']}, {
+                                          "$set": {kpi + "_normal": 0}})
 
-#### ImplÃƒÂ©mentation de TOPSIS
+# Implementation de TOPSIS
 
-### Meilleur influenceur : (1, 1, ...) * weights
-### Pire influenceur : (0, 0, ...)
+# Meilleur influenceur : (1, 1, ...) * weights
+# Pire influenceur : (0, 0, ...)
 
 kpi_normal = ["page_fans_normal", "page_negative_feedback_unique_normal", "page_engagement_rate_normal",
               "page_posts_engagement_rate_normal", "reach_rate_normal", "fan_adds_rate_normal",
-              "fan_adds_removes_rate_normal", "verification_status_normal","page_video_views_normal","vu_unique_rate_normal","vu_click_rate_normal","vu_complete_30s_rate_normal"]
+              "fan_adds_removes_rate_normal", "verification_status_normal", "page_video_views_normal", "vu_unique_rate_normal", "vu_click_rate_normal", "vu_complete_30s_rate_normal"]
 
 weights = [0.2, 0.05, 0.05, 0.15, 0.15, 0.1, 0.1, 0.1, 0.05, 0.05]
 
@@ -201,23 +228,24 @@ for inf in collection.find():
     for k in kpi_normal:
         try:
             for w in weights:
-                collection.update_one({"_id": inf['_id']}, {"$set": {k + "_pond": inf[k] * w}})
+                collection.update_one({"_id": inf['_id']}, {
+                                      "$set": {k + "_pond": inf[k] * w}})
         except:
-            collection.update_one({"_id": inf['_id']}, {"$set": {k + '_pond': '0'}})
+            collection.update_one({"_id": inf['_id']}, {
+                                  "$set": {k + '_pond': '0'}})
 
-### Meilleur influenceur: (max-min/max-min=1)
+# Meilleur influenceur: (max-min/max-min=1)
 
 # best_alt = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]*weights
 
-best_alt = [0.2, 0.05, 0.05, 0.15, 0.15, 0.1, 0.1, 0.1,0.1,0.1,0.1,0.1]
+best_alt = [0.2, 0.05, 0.05, 0.15, 0.15, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 
-### pire influenceur: (min-min/max-min=1)
+# pire influenceur: (min-min/max-min=1)
 
-worst_alt = [0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0]
+worst_alt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
-
-##Calcul du score de Topsis
+# Calcul du score de Topsis
 
 for inf in collection.find():
 
@@ -246,10 +274,12 @@ for inf in collection.find():
 
         score = (s_moins / (s_moins + s_plus)) * 100
 
-        collection.update_one({"_id": inf['_id']}, {"$set": {"facebook_score": round(score,2)}})
-        influence_account.update_one({"page_fb": inf["_id"]}, {"$set": {"facebook_score": round(score,2)}})
+        collection.update_one({"_id": inf['_id']}, {
+                              "$set": {"facebook_score": round(score, 2)}})
+        influence_account.update_one({"page_fb": inf["_id"]}, {
+                                     "$set": {"facebook_score": round(score, 2)}})
 
     else:
         collection.update_one({"_id": inf['_id']}, {"$set": {"score": 0}})
-        influence_account.update_one({"page_fb": inf["_id"]}, {"$set": {"facebook_score": round(score, 2)}})
-
+        influence_account.update_one({"page_fb": inf["_id"]}, {
+                                     "$set": {"facebook_score": round(score, 2)}})
--- global_scoring.py
+++ global_scoring.py
@@ -2,19 +2,19 @@ from pymongo import MongoClient
 import pandas as pd
 import re
 from bson import regex
+import config
+
+# Script de calcul du score sur la plateforme Twitter
+
+# Connection au cluster :
+
+# username = "victor"  ## Identifiant de connection à modifier
+# mdp = "lataps"
+
+# cluster = f"mongodb+srv://{username}:{mdp}@cluster0.bxvcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
-##### Script de calcul du score sur la plateforme Twitter
-
-#### Connection au cluster :
-
-username = "victor"  ## Identifiant de connection à modifier
-mdp = "lataps"
-
-cluster = f"mongodb+srv://{username}:{mdp}@cluster0.bxvcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-
-cluster = MongoClient(cluster)
+cluster = MongoClient(config.MONGO_PASSPHRASE)
 
 
 db = cluster["classed"]
@@ -24,26 +24,23 @@ influencer_account = db["influencer_account"]
 networks = ["facebook, instagram, twitter, youtube, tiktok"]
 
 
+# Affectation à une division globale
 
-#### Affectation à une division globale
-
-#### La division globale est choisie en fonction de la plateforme sur laquelle l'influenceur a le plus d'abonnés...
+# La division globale est choisie en fonction de la plateforme sur laquelle l'influenceur a le plus d'abonnés...
 
 
 for influencer in influencer_account.find():
-
 
     pipeline = [
 
         {"$group": {
             "_id": "_id",
-            "max": {"$max": "$" },
-            "min": {"$min": "$" }
+            "max": {"$max": "$"},
+            "min": {"$min": "$"}
         }
         }
 
-    ];
-
+    ]
 
     max_followers = 0
     div = 0
@@ -55,17 +52,14 @@ for influencer in influencer_account.find():
             max_followers = influencer[network + "_followers"]
             div = influencer[network + "_division"]
 
-
     influencer["division_globale"] = div
 
-
     division_globale = 0
-
 
     influencer["division_globale"] = division_globale
 
 
-#### Calcul du score pondéré
+# Calcul du score pondéré
 
 
 #### FORMULE : SOMMME(score_p * followers_p) / SOMME(followers_p)
@@ -77,20 +71,13 @@ for influencer in influencer_account.find():
 
     score_global = 0
 
-
     for network in networks:
 
         sum_followers = sum_followers + influencer[network + "_followers"]
 
-        score_global = score_global + influencer[network + "_followers"] * influencer[network + "_score"]
-
+        score_global = score_global + \
+            influencer[network + "_followers"] * influencer[network + "_score"]
 
     score_global = score_global / sum_followers
 
-
     influencer["score_global"] = score_global
-
-
-
-
-
